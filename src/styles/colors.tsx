b52d9a07b41488f06f17d246e1e6bddefd6e6a5f
Yasser Shkeir <shkeiryasser@gmail.com>
2023-03-03 00:19:17 +0200

    Styles: Add Colors

---
+++ src/styles/colors.tsx
@@ -0,0 +1,9 @@
+export const colors = {
+  primary80: "#69e6f0",
+  primary100: "#40a3c0",
+  secondary80: "#cfe8ff",
+  secondary100: "#80d9ff",
+  white: "#fff",
+  black: "#000",
+  gray: "#757575",
+};
